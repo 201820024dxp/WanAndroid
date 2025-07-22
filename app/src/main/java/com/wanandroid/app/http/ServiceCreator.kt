f98625a40f94b1ac573ceb64783d091b268b578b
dangxupeng <dangxupeng@drojian.dev>
2025-07-22 19:02:39 +0800

    修改：设置项目列表item收藏图标的点击背景 新增：网络请求与响应的日志监听

--- app/src/main/java/com/wanandroid/app/http/ServiceCreator.kt
+++ app/src/main/java/com/wanandroid/app/http/ServiceCreator.kt
@@ -1,5 +1,7 @@
 package com.wanandroid.app.http
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.jackson.JacksonConverterFactory
@@ -8,10 +10,20 @@ object ServiceCreator {
 
     const val BASE_URL = "https://www.wanandroid.com/"
 
+    // 创建 HttpLoggingInterceptor 实例
+    val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY  // 设置日志级别为 BODY，记录所有请求和响应的详细信息
+    }
+
+    // 创建 OkHttpClient 实例并添加拦截器
+    val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)  // 添加日志拦截器
+        .build()
+
     val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
-//        .addConverterFactory(JacksonConverterFactory.create())
+        .client(okHttpClient)
         .build()
 
     //    val placeService = retrofit.create(PlaceService::class.java)
