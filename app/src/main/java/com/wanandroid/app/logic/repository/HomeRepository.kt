946bdc7c27fd915b3206a14a567241b2053d4b6c
dangxupeng <dangxupeng@drojian.dev>
2025-07-23 17:08:43 +0800

    修改：首页列表的实现方式，使用多类型RecyclerView优化滑动冲突问题。

--- app/src/main/java/com/wanandroid/app/logic/repository/HomeRepository.kt
+++ app/src/main/java/com/wanandroid/app/logic/repository/HomeRepository.kt
@@ -1,21 +1,19 @@
 package com.wanandroid.app.logic.repository
 
+import androidx.lifecycle.liveData
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import com.wanandroid.app.base.IntKeyPagingSource
-import com.wanandroid.app.http.NetworkResponse
 import com.wanandroid.app.logic.model.Article
-import com.wanandroid.app.logic.model.PageResponse
 import com.wanandroid.app.logic.network.impl.HomeServiceNetwork
-import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.async
 import kotlinx.coroutines.supervisorScope
 
 object HomeRepository {
 
-    suspend fun getBanner() = HomeServiceNetwork.getBanner()
+    fun getBanner() = liveData { emit(HomeServiceNetwork.getBanner().data ?: emptyList()) }
 
-    suspend fun getArticleTopList() = HomeServiceNetwork.getArticleTopList()
+    private suspend fun getArticleTopList() = HomeServiceNetwork.getArticleTopList()
 
     fun getArticlePageList(pageSize: Int) =
         Pager(
@@ -28,21 +26,25 @@ object HomeRepository {
             IntKeyPagingSource(
                 pageStart = 0,      // 首页文章列表从第0页开始
                 block = { page, pageSize ->
-                    val response = ArrayList<Article>()
-                    supervisorScope {
-                        // 使用async并行请求
-                        val topsDeferred = async { getArticleTopList() }
-                        val articlesDeferred =
-                            async { HomeServiceNetwork.getArticlePageList(page, pageSize) }
+                    if (page == 0) {        // 如果是第一页，则加载置顶文章和普通文章
+                        val response = ArrayList<Article>()
+                        supervisorScope {
+                            // 使用async并行请求
+                            val topsDeferred = async { getArticleTopList() }
+                            val articlesDeferred =
+                                async { HomeServiceNetwork.getArticlePageList(page, pageSize) }
 
-                        // 获取置顶文章和普通文章
-                        val tops = topsDeferred.await().data ?: emptyList()
-                        val articles = articlesDeferred.await().data?.datas ?: emptyList()
+                            // 获取置顶文章和普通文章
+                            val tops = topsDeferred.await().data ?: emptyList()
+                            val articles = articlesDeferred.await().data?.datas ?: emptyList()
 
-                        response.addAll(tops)
-                        response.addAll(articles)
+                            response.addAll(tops)
+                            response.addAll(articles)
+                        }
+                        response
+                    } else {                // 之后的页数只需要加载普通文章即可
+                        HomeServiceNetwork.getArticlePageList(page, pageSize).data?.datas ?: emptyList()
                     }
-                    response
                 }
             )
         }.flow      // 返回一个Flow对象
--- app/src/main/java/com/wanandroid/app/ui/home/child/explore/ExploreViewModel.kt
+++ app/src/main/java/com/wanandroid/app/ui/home/child/explore/ExploreViewModel.kt
@@ -2,30 +2,20 @@ package com.wanandroid.app.ui.home.child.explore
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
-import com.wanandroid.app.logic.model.Banner
 import com.wanandroid.app.logic.repository.HomeRepository
-import kotlinx.coroutines.launch
 
 class ExploreViewModel : ViewModel() {
 
     // 首页 Banner LiveData
-    private var _bannerList = MutableLiveData<List<Banner>>()
-    val bannerList get() = _bannerList
+    private val _bannerList = MutableLiveData<Any>()
+    val bannerList = _bannerList.switchMap { HomeRepository.getBanner() }
 
     // 首页列表数据 Flow
     val getArticlesFlow = HomeRepository.getArticlePageList(20).cachedIn(viewModelScope)    // cachedIn()操作符使数据流可共享
 
-    fun getBanner() {
-        viewModelScope.launch {
-            try {
-                val banners = HomeRepository.getBanner().data ?: emptyList()
-                _bannerList.value = banners
-            } catch (e: Exception) {
-                throw RuntimeException("获取Banner失败", e)
-            }
-        }
-    }
-
+    // 手动拉取首页数据
+    fun getBanner() { _bannerList.value = _bannerList.value }
 }
